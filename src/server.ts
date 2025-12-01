f937d7b3eb8ed76eb943a61425dab7f45ee339a1
cm82tj <orosz.laszlo@macleod.hu>
2025-12-01 13:42:48 +0100

    Hello üzenet módosítása feature branch-ben

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (_req, res) => {
-  res.send("Hello DevOps + teszt");
+  res.send("Hello DevOps + teszt_v2");
 });
 
 app.listen(PORT, () => {
